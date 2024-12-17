5876779aa0f6895a4c43441c3c072b953890a65e
Matt Hull <hull.matt@gmail.com>
2024-12-16 19:21:59 -0600

    sunburst

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,7 +7,6 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/viz", under: "viz"
 pin "d3" # @7.9.0
-pin "d3-array" # @2.12.1
 pin "d3-axis" # @3.0.0
 pin "d3-brush" # @3.0.0
 pin "d3-chord" # @3.0.1
@@ -24,14 +23,12 @@ pin "d3-format" # @3.1.0
 pin "d3-geo" # @3.1.1
 pin "d3-hierarchy" # @3.1.2
 pin "d3-interpolate" # @3.0.1
-pin "d3-path" # @1.0.9
 pin "d3-polygon" # @3.0.1
 pin "d3-quadtree" # @3.0.1
 pin "d3-random" # @3.0.1
 pin "d3-scale" # @4.0.2
 pin "d3-scale-chromatic" # @3.1.0
 pin "d3-selection" # @3.0.0
-pin "d3-shape" # @1.3.7
 pin "d3-time" # @3.1.0
 pin "d3-time-format" # @4.1.0
 pin "d3-timer" # @3.0.1
@@ -40,4 +37,6 @@ pin "d3-zoom" # @3.0.0
 pin "delaunator" # @5.0.1
 pin "internmap" # @2.0.3
 pin "robust-predicates" # @3.0.2
-pin "d3-sankey" # @0.12.3
+pin "d3-array" # @3.2.4
+pin "d3-path" # @3.1.0
+pin "d3-shape" # @3.2.0
